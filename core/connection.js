066891791afde58d5173c65da53ea405151aa4d8
Ashime Sho <a_2oo+gituseremail@outlook.com>
2024-09-20 20:12:38 -0400

    Revert "Hello darkness my old friend"
    This reverts commit 2d649d0dd4e75c4bbb9e58a97d84109c5d79e25c.

--- core/connection.js
+++ core/connection.js
@@ -27,7 +27,6 @@
 goog.provide('Blockly.Connection');
 
 goog.require('Blockly.Events.BlockMove');
-goog.require('Blockly.CustomShapes');
 
 goog.require('goog.asserts');
 goog.require('goog.dom');
@@ -715,9 +714,6 @@ Blockly.Connection.prototype.getOutputShape = function() {
   }
   if (this.check_.indexOf('Object') !== -1) {
     return Blockly.OUTPUT_SHAPE_OBJECT;
-  }
-  if (Blockly.CustomShapes.has(this.check_)) {
-    return Blockly.CustomShapes.get(this.check_).shape.name;
   }
   return Blockly.OUTPUT_SHAPE_ROUND;
 };
