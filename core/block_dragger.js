4fbf76aafd6ddb83b78c8bcffa6a192304fd0e2c
LilyMakesThings <vgmhut@gmail.com>
2026-04-21 02:58:22 +0100

    Revert "smart extendable blocks"
    This reverts commit 2d094afb729a8b337e7301eefabad9733b44b567.

--- core/block_dragger.js
+++ core/block_dragger.js
@@ -27,7 +27,6 @@
 goog.provide('Blockly.BlockDragger');
 
 goog.require('Blockly.BlockAnimations');
-goog.require('Blockly.ExtenderMutation');
 goog.require('Blockly.Events.BlockMove');
 goog.require('Blockly.Events.DragBlockOutside');
 goog.require('Blockly.Events.EndBlockDrag');
@@ -107,13 +106,6 @@ Blockly.BlockDragger = function(block, workspace) {
    * @private
    */
   this.dragIconData_ = Blockly.BlockDragger.initIconData_(block);
-
-  /**
-   * Blocks whose extendable tail state should be managed for this drag.
-   * @type {!Array<!Blockly.BlockSvg>}
-   * @private
-   */
-  this.autoExtendCandidates_ = [];
 };
 
 /**
@@ -125,7 +117,6 @@ Blockly.BlockDragger.prototype.dispose = function() {
   this.workspace_ = null;
   this.startWorkspace_ = null;
   this.dragIconData_.length = 0;
-  this.autoExtendCandidates_.length = 0;
 
   if (this.draggedConnectionManager_) {
     this.draggedConnectionManager_.dispose();
@@ -174,10 +165,6 @@ Blockly.BlockDragger.prototype.startBlockDrag = function(currentDragDeltaXY) {
   this.workspace_.setResizesEnabled(false);
   Blockly.BlockAnimations.disconnectUiStop();
 
-  this.autoExtendCandidates_ = [];
-  this.registerAutoExtendCandidateForDrag_(this.draggingBlock_);
-
-  var parentBeforeUnplug = this.draggingBlock_.getParent();
   if (this.draggingBlock_.getParent()) {
     this.draggingBlock_.unplug();
     var delta = this.pixelsToWorkspaceUnits_(currentDragDeltaXY);
@@ -186,9 +173,7 @@ Blockly.BlockDragger.prototype.startBlockDrag = function(currentDragDeltaXY) {
     this.draggingBlock_.translate(newLoc.x, newLoc.y);
     Blockly.BlockAnimations.disconnectUiEffect(this.draggingBlock_);
   }
-  this.registerAutoExtendCandidateForDrag_(parentBeforeUnplug);
   this.draggingBlock_.setDragging(true);
-
   // For future consideration: we may be able to put moveToDragSurface inside
   // the block dragger, which would also let the block not track the block drag
   // surface.
@@ -221,12 +206,6 @@ Blockly.BlockDragger.prototype.dragBlock = function(e, currentDragDeltaXY) {
   this.deleteArea_ = this.workspace_.isDeleteArea(e);
   var isOutside = !this.workspace_.isInsideBlocksArea(e);
   this.draggedConnectionManager_.update(delta, this.deleteArea_, isOutside);
-
-  var previewClosestConnection = this.getClosestPreviewConnection_();
-  this.maybeRegisterPreviewAutoExtendCandidate_(previewClosestConnection);
-  this.maybeAutoExtendTailFromGeneralProximity_(previewClosestConnection);
-  this.maybeAutoExtendTailFromPreviewConnection_(previewClosestConnection);
-
   if (isOutside !== this.wasOutside_) {
     this.fireDragOutsideEvent_(isOutside);
     this.wasOutside_ = isOutside;
@@ -283,20 +262,14 @@ Blockly.BlockDragger.prototype.endBlockDrag = function(e, currentDragDeltaXY) {
     this.draggingBlock_.moveConnections_(delta.x, delta.y);
     this.draggingBlock_.setDragging(false);
     this.fireMoveEvent_();
-    var willConnect = this.draggedConnectionManager_.wouldConnectBlock();
-    if (willConnect) {
+    if (this.draggedConnectionManager_.wouldConnectBlock()) {
       // Applying connections also rerenders the relevant blocks.
       this.draggedConnectionManager_.applyConnections();
     } else {
       this.draggingBlock_.render();
     }
-    var reconcileBlocks = this.getAutoExtendedBlocksForReconcile_();
-    this.maybeRetractUnusedAutoTailAfterDrag_();
-    this.scheduleAutoExtendedTailShadowReconcile_(reconcileBlocks);
     this.draggingBlock_.scheduleSnapAndBump();
   }
-
-  this.clearAutoExtendDragState_();
   this.workspace_.setResizesEnabled(true);
 
   var toolbox = this.workspace_.getToolbox();
@@ -344,422 +317,6 @@ Blockly.BlockDragger.prototype.endBlockDrag = function(e, currentDragDeltaXY) {
       ws.refreshToolboxSelection_();
     });
   }
-};
-
-/**
- * Whether this block supports modern extendable tail mutation operations.
- * @param {!Blockly.BlockSvg} block The block to test.
- * @return {boolean} True if the block supports auto tail extension.
- * @private
- */
-Blockly.BlockDragger.prototype.isAutoExtendCandidate_ = function(block) {
-  return !!(block &&
-      block.extendDefinitions_ &&
-      Array.isArray(block.argumentIds_) &&
-      typeof block.insertInputsAtIndex === 'function');
-};
-
-/**
- * Whether a block is in the currently dragged stack.
- * @param {?Blockly.BlockSvg} block The block to test.
- * @return {boolean} True if the block is the dragged block or one of its descendants.
- * @private
- */
-Blockly.BlockDragger.prototype.isInDraggedStack_ = function(block) {
-  if (!block || !this.draggingBlock_) {
-    return false;
-  }
-  return block === this.draggingBlock_ || block.getRootBlock() === this.draggingBlock_;
-};
-
-/**
- * Get the real (non-shadow) block connected to the current last dynamic input.
- * @param {!Blockly.BlockSvg} block The block to inspect.
- * @return {?Blockly.BlockSvg} Connected non-shadow block, or null.
- * @private
- */
-Blockly.BlockDragger.prototype.getLastRealTailTarget_ = function(block) {
-  var argumentIds = block.argumentIds_ || [];
-  if (!argumentIds.length) {
-    return null;
-  }
-
-  var input = block.getInput(argumentIds[argumentIds.length - 1]);
-  if (!input || !input.connection) {
-    return null;
-  }
-
-  var target = input.connection.targetBlock();
-  if (!target) {
-    return null;
-  }
-
-  return (target.isShadow && target.isShadow()) ? null : target;
-};
-
-/**
- * Get the currently previewed closest connection from the insertion marker manager.
- * @return {?Blockly.RenderedConnection} Closest preview connection, if any.
- * @private
- */
-Blockly.BlockDragger.prototype.getClosestPreviewConnection_ = function() {
-  return this.draggedConnectionManager_ ?
-      (this.draggedConnectionManager_.closestConnection_ || null) : null;
-};
-
-/**
- * If a preview targets an extendable block, ensure it is tracked for this drag.
- * @param {?Blockly.RenderedConnection} previewConnection The preview connection.
- * @private
- */
-Blockly.BlockDragger.prototype.maybeRegisterPreviewAutoExtendCandidate_ = function(previewConnection) {
-  if (!previewConnection || !previewConnection.sourceBlock_) {
-    return;
-  }
-  if (this.isInDraggedStack_(previewConnection.sourceBlock_)) {
-    return;
-  }
-  this.registerAutoExtendCandidateForDrag_(previewConnection.sourceBlock_);
-};
-
-/**
- * Whether a connection maps to the host block's current last dynamic input.
- * @param {!Blockly.BlockSvg} block The host block.
- * @param {!Blockly.RenderedConnection} connection The candidate connection.
- * @return {boolean} True when the connection belongs to the last dynamic socket.
- * @private
- */
-Blockly.BlockDragger.prototype.isLastDynamicInputConnection_ = function(block, connection) {
-  if (!this.isAutoExtendCandidate_(block) || !connection) {
-    return false;
-  }
-  var argumentIds = block.argumentIds_ || [];
-  if (!argumentIds.length) {
-    return false;
-  }
-  var input = block.getInput(argumentIds[argumentIds.length - 1]);
-  return !!(input && input.connection && input.connection === connection);
-};
-
-/**
- * Whether a host block is eligible for one-shot auto-extension this drag.
- * Must have a non-shadow block in its current last dynamic input.
- * @param {!Blockly.BlockSvg} hostBlock The extendable host block.
- * @return {boolean} True if the host should auto-extend now.
- * @private
- */
-Blockly.BlockDragger.prototype.shouldAutoExtendHostBlock_ = function(hostBlock) {
-  return !!(this.isAutoExtendCandidate_(hostBlock) &&
-  !this.isInDraggedStack_(hostBlock) &&
-      !hostBlock.autoExtendDidInsert_ &&
-      this.getLastRealTailTarget_(hostBlock));
-};
-
-/**
- * Auto-extend as soon as the drag preview targets the last dynamic input.
- * @param {?Blockly.RenderedConnection} previewConnection The preview connection.
- * @private
- */
-Blockly.BlockDragger.prototype.maybeAutoExtendTailFromPreviewConnection_ = function(previewConnection) {
-  if (!previewConnection || !previewConnection.sourceBlock_) {
-    return;
-  }
-
-  var hostBlock = previewConnection.sourceBlock_;
-  if (!this.shouldAutoExtendHostBlock_(hostBlock)) {
-    return;
-  }
-
-  if (!this.isLastDynamicInputConnection_(hostBlock, previewConnection)) {
-    return;
-  }
-
-  var beforeLen = hostBlock.argumentIds_.length;
-  hostBlock.insertInputsAtIndex(hostBlock.argumentIds_.length + 1, {});
-  var delta = hostBlock.argumentIds_.length - beforeLen;
-  if (delta > 0) {
-    if (!Array.isArray(hostBlock.autoExtendDragDeltas_)) {
-      hostBlock.autoExtendDragDeltas_ = [];
-    }
-    hostBlock.autoExtendDragDeltas_.push(delta);
-    hostBlock.autoExtendDidInsert_ = true;
-    this.ensureTailShadowIfNeeded_(hostBlock);
-  }
-};
-
-/**
- * Check whether the dragged block is near the host block's last dynamic input.
- * Uses a larger radius than snap preview so extension appears before precise hover.
- * @param {!Blockly.BlockSvg} hostBlock The extendable host block.
- * @return {boolean} True if dragged block is within general vicinity.
- * @private
- */
-Blockly.BlockDragger.prototype.isNearLastDynamicInput_ = function(hostBlock) {
-  if (!hostBlock || !this.draggingBlock_ || !this.draggingBlock_.outputConnection) {
-    return false;
-  }
-
-  var argumentIds = hostBlock.argumentIds_ || [];
-  if (!argumentIds.length) {
-    return false;
-  }
-
-  var input = hostBlock.getInput(argumentIds[argumentIds.length - 1]);
-  if (!input || !input.connection) {
-    return false;
-  }
-
-  // Dragged block anchor: edge that faces the host extension side.
-  var draggedBounds = this.draggingBlock_.getBoundingRectangle && this.draggingBlock_.getBoundingRectangle();
-  if (!draggedBounds || !draggedBounds.topLeft || !draggedBounds.bottomRight) {
-    return false;
-  }
-
-  // Host reference: extending edge center (right in LTR, left in RTL).
-  var hostBounds = hostBlock.getBoundingRectangle && hostBlock.getBoundingRectangle();
-  if (!hostBounds || !hostBounds.topLeft || !hostBounds.bottomRight) {
-    return false;
-  }
-
-  var hostEdgeX = this.workspace_.RTL ? hostBounds.topLeft.x : hostBounds.bottomRight.x;
-  var hostEdgeY = (hostBounds.topLeft.y + hostBounds.bottomRight.y) / 2;
-
-  var draggedCenterX = (draggedBounds.topLeft.x + draggedBounds.bottomRight.x) / 2;
-  var anchorX = hostEdgeX >= draggedCenterX ? draggedBounds.bottomRight.x : draggedBounds.topLeft.x;
-  var anchorY = (draggedBounds.topLeft.y + draggedBounds.bottomRight.y) / 2;
-
-  // Padded vicinity box around host extending edge.
-  var halfW = Math.max(34, (Blockly.SNAP_RADIUS * 2.4));
-  var halfH = Math.max(24, (Blockly.SNAP_RADIUS * 1.6));
-  var minX = hostEdgeX - halfW;
-  var maxX = hostEdgeX + halfW;
-  var minY = hostEdgeY - halfH;
-  var maxY = hostEdgeY + halfH;
-
-  return anchorX >= minX && anchorX <= maxX && anchorY >= minY && anchorY <= maxY;
-};
-
-/**
- * Auto-extend if the dragged block is generally near the host's last dynamic input.
- * @private
- */
-Blockly.BlockDragger.prototype.maybeAutoExtendTailFromGeneralProximity_ = function(previewConnection) {
-  this.maybeRetractTailWhenLeavingProximity_(previewConnection);
-
-  var blocks = this.workspace_.getAllBlocks(false);
-  for (var i = 0; i < blocks.length; i++) {
-    var hostBlock = blocks[i];
-    if (!this.shouldAutoExtendHostBlock_(hostBlock)) {
-      continue;
-    }
-    if (!this.isNearLastDynamicInput_(hostBlock)) {
-      continue;
-    }
-
-    this.registerAutoExtendCandidateForDrag_(hostBlock);
-
-    var beforeLen = hostBlock.argumentIds_.length;
-    hostBlock.insertInputsAtIndex(hostBlock.argumentIds_.length + 1, {});
-    var delta = hostBlock.argumentIds_.length - beforeLen;
-    if (delta > 0) {
-      if (!Array.isArray(hostBlock.autoExtendDragDeltas_)) {
-        hostBlock.autoExtendDragDeltas_ = [];
-      }
-      hostBlock.autoExtendDragDeltas_.push(delta);
-      hostBlock.autoExtendDidInsert_ = true;
-      this.ensureTailShadowIfNeeded_(hostBlock);
-    }
-  }
-};
-
-/**
- * Retract an auto-added tail while dragging if the dragged block leaves vicinity
- * and the tail remains unused.
- * @param {?Blockly.RenderedConnection} previewConnection The current preview connection.
- * @private
- */
-Blockly.BlockDragger.prototype.maybeRetractTailWhenLeavingProximity_ = function(previewConnection) {
-  if (typeof Blockly.ExtenderMutation === 'undefined') {
-    return;
-  }
-
-  for (var i = 0; i < this.autoExtendCandidates_.length; i++) {
-    var block = this.autoExtendCandidates_[i];
-    if (!this.isAutoExtendCandidate_(block) ||
-        !block.autoExtendDidInsert_ ||
-        !Array.isArray(block.autoExtendDragDeltas_) ||
-        !block.autoExtendDragDeltas_.length) {
-      continue;
-    }
-
-    var previewTargetsBlock = !!(previewConnection &&
-        previewConnection.sourceBlock_ === block &&
-        this.isLastDynamicInputConnection_(block, previewConnection));
-    var stillNear = this.isNearLastDynamicInput_(block) || previewTargetsBlock;
-    if (stillNear || this.getLastRealTailTarget_(block)) {
-      continue;
-    }
-
-    var removeCount = block.autoExtendDragDeltas_.pop();
-    var minInputs = Blockly.ExtenderMutation.getMinInputCount_(block);
-    Blockly.ExtenderMutation.removeTailInputs_.call(block, removeCount, minInputs);
-    block.autoExtendDidInsert_ = false;
-  }
-};
-
-/**
- * Retract any trailing auto-added tail slot that is still empty/shadow.
- * @private
- */
-Blockly.BlockDragger.prototype.maybeRetractUnusedAutoTailAfterDrag_ = function() {
-  if (typeof Blockly.ExtenderMutation === 'undefined') {
-    return;
-  }
-
-  for (var i = 0; i < this.autoExtendCandidates_.length; i++) {
-    var block = this.autoExtendCandidates_[i];
-    if (!this.isAutoExtendCandidate_(block) ||
-        !Array.isArray(block.autoExtendDragDeltas_) ||
-        !block.autoExtendDragDeltas_.length) {
-      continue;
-    }
-
-    while (block.autoExtendDragDeltas_.length) {
-      // Keep auto-added inputs that are actually used.
-      if (this.getLastRealTailTarget_(block)) {
-        break;
-      }
-
-      var removeCount = block.autoExtendDragDeltas_.pop();
-      var minInputs = Blockly.ExtenderMutation.getMinInputCount_(block);
-      Blockly.ExtenderMutation.removeTailInputs_.call(block, removeCount, minInputs);
-    }
-  }
-};
-
-/**
- * Snapshot blocks that were auto-extended this drag for post-drag reconciliation.
- * @return {!Array<!Blockly.BlockSvg>} Blocks that need tail shadow reconciliation.
- * @private
- */
-Blockly.BlockDragger.prototype.getAutoExtendedBlocksForReconcile_ = function() {
-  var blocks = [];
-  for (var i = 0; i < this.autoExtendCandidates_.length; i++) {
-    var block = this.autoExtendCandidates_[i];
-    if (block && block.autoExtendDidInsert_) {
-      blocks.push(block);
-    }
-  }
-  return blocks;
-};
-
-/**
- * Delay shadow reconciliation until drag state has fully settled.
- * @param {!Array<!Blockly.BlockSvg>} candidateBlocks Candidate host blocks snapshot.
- * @private
- */
-Blockly.BlockDragger.prototype.scheduleAutoExtendedTailShadowReconcile_ = function(candidateBlocks) {
-  var self = this;
-  var attemptReconcile = function(triesLeft) {
-    if (self.workspace_ && self.workspace_.isDragging && self.workspace_.isDragging() && triesLeft > 0) {
-      goog.Timer.callOnce(function() {
-        attemptReconcile(triesLeft - 1);
-      }, 25);
-      return;
-    }
-
-    for (var i = 0; i < candidateBlocks.length; i++) {
-      var block = candidateBlocks[i];
-      if (!block) {
-        continue;
-      }
-      if (typeof block.updateDisplay_ === 'function') {
-        block.updateDisplay_();
-      }
-      self.ensureTailShadowIfNeeded_(block);
-    }
-  };
-
-  goog.Timer.callOnce(function() {
-    attemptReconcile(6);
-  }, 0);
-};
-
-/**
- * If the last dynamic input should have a shadow but doesn't, create it.
- * @param {!Blockly.BlockSvg} block The extendable host block.
- * @private
- */
-Blockly.BlockDragger.prototype.ensureTailShadowIfNeeded_ = function(block) {
-  if (!this.isAutoExtendCandidate_(block) || !block.argumentIds_.length) {
-    return;
-  }
-
-  var lastIndex = block.argumentIds_.length - 1;
-  var input = block.getInput(block.argumentIds_[lastIndex]);
-  if (!input || !input.connection || input.connection.targetBlock()) {
-    return;
-  }
-
-  var definition = Blockly.ExtenderMutation.getInputDefinitionFromIndex_(lastIndex, block);
-  if (!definition || !definition.shadow) {
-    return;
-  }
-
-  var prevRecordUndo = Blockly.Events.recordUndo;
-  Blockly.Events.recordUndo = false;
-  try {
-    var shadowBlock = block.workspace.newBlock(definition.shadow);
-    if (definition.field) {
-      var defaultValue = definition.defaultValue;
-      if (defaultValue === null || typeof defaultValue === 'undefined') {
-        defaultValue = Blockly.ExtenderMutation.getShadowFieldDefault_(definition.shadow, definition.field);
-      }
-      shadowBlock.setFieldValue(defaultValue, definition.field);
-    }
-    shadowBlock.setShadow(true);
-    shadowBlock.initSvg();
-    shadowBlock.render(false);
-    if (shadowBlock.outputConnection) {
-      shadowBlock.outputConnection.connect(input.connection);
-    }
-    if (!input.connection.targetConnection) {
-      shadowBlock.dispose();
-    } else {
-      input.connection.setShadowDom(Blockly.Xml.blockToDom(shadowBlock));
-    }
-  } finally {
-    Blockly.Events.recordUndo = prevRecordUndo;
-  }
-};
-
-/**
- * Clear temporary per-drag auto-extend bookkeeping state.
- * @private
- */
-Blockly.BlockDragger.prototype.clearAutoExtendDragState_ = function() {
-  for (var i = 0; i < this.autoExtendCandidates_.length; i++) {
-    this.autoExtendCandidates_[i].autoExtendDragDeltas_ = null;
-    this.autoExtendCandidates_[i].autoExtendDidInsert_ = null;
-  }
-  this.autoExtendCandidates_ = [];
-};
-
-/**
- * Register a block as a drag-scoped candidate for auto tail extension.
- * @param {?Blockly.BlockSvg} block The block to track.
- * @private
- */
-Blockly.BlockDragger.prototype.registerAutoExtendCandidateForDrag_ = function(block) {
-  if (!block || !this.isAutoExtendCandidate_(block)) {
-    return;
-  }
-  if (this.autoExtendCandidates_.indexOf(block) !== -1) {
-    return;
-  }
-  block.autoExtendDragDeltas_ = [];
-  block.autoExtendDidInsert_ = false;
-  this.autoExtendCandidates_.push(block);
 };
 
 /**
